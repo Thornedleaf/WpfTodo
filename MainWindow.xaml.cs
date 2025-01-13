78880ce8d6558174401c81df15cfa3f9869da2c4
Thornedleaf <oskar.josefsson.webd24jon@edu.tucsweden.se>
2025-01-13 12:00:33 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTodo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Todolist _todolist;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _todolist = new Todolist();
+        }
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string task = TaskTextBox.Text;
+            if (!string.IsNullOrEmpty(task))
+            {
+                _todolist.AddTask(task);
+
+            }
+
+
+        }
+        private void UpdateTaskList()
+        {
+            TasksListBox.Items.Clear();
+            foreach (var task in _todolist.GetAllTasks())
+            {
+                TasksListBox.Items.Add(task);
+            }
+
+        }
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TasksListBox.SelectedIndex >= 0)
+            {
+                _todolist.RemoveTask(TasksListBox.SelectedIndex);
+                UpdateTaskList();
+            }
+        }
+    }
+}
---
+++ Todolist.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfTodo
+{
+    public class Todolist
+    {
+        private List<string> tasks = new List<string>();
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+        public void RemoveTask(int index)
+        {
+            if (index >= 0 && index < tasks.Count)
+            {
+                tasks.RemoveAt(index);
+            }
+        }
+        public List<string> GetAllTasks()
+        {
+            return tasks;
+        }
+
+    }
+}
